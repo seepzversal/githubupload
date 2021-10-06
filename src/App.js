d8dc73d4871eac0712a26cc3c0fbec6e7ef1b1fb
seepmaini <seepmaini14@gmail.com>
2021-10-06 18:21:37 +0530

    My first react app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import TodoItem from "./TodoItem"
+import todosData from "./todosData"
+//class component with a constructor method and the state change.
+class App extends React.Component{
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    constructor(){
+        super()
+        this.state = {
+            todos: todosData
+        }
+        this.handleChange=this.handleChange.bind(this)//binding of the function inside the class component
+    }
+    handleChange(id){
+        this.setState(prevState =>{
+            const updateTodos=prevState.todos.map(todo=>{
+                if(todo.id==id){
+                    return {
+                        ...todo,
+                        completed: !todo.completed
+                    }
+                }
+                return todo
+            })
+            return {
+                
+                todos:updateTodos
+                
+            }
+        })
+    }
+
+
+    render(){
+        const todoItems = this.state.todos.map(item => <TodoItem key={item.id} item={item}
+        handleChange={this.handleChange}/>)
+    
+        return (
+            <div>
+            <h1 class="h1">My First App in React---To-Do List </h1>
+            <div className="todo-list">
+                {todoItems}
+            </div>
+            </div>
+        )
+    }
+   
 }
 
-export default App;
+export default App
---
+++ src/todosData.js
@@ -0,0 +1,55 @@
+const todosData = [
+    {
+        id: 1,
+        text: "Read 10 pages of a Book",
+        completed: true
+    },
+    {
+        id: 2,
+        text: "Grocery shopping",
+        completed: false
+    },
+    {
+        id: 3,
+        text: "Cleaning your cupboard",
+        completed: false
+    },
+    {
+        id: 4,
+        text: "Learn JavaScript",
+        completed: true
+    },
+    {
+        id: 5,
+        text: "Do 2 topics of React",
+        completed: false
+    },
+    {
+        id: 6,
+        text: "Learn Git and Github",
+        completed: false
+    },
+    {
+        id: 7,
+        text: "Cook Food",
+        completed: false
+    },
+    {
+        id: 8,
+        text: "Not to eat junk food",
+        completed: true
+    },
+    {
+        id: 9,
+        text: "Dusting",
+        completed: true
+    },
+    {
+        id: 10,
+        text: "Going on Walk",
+        completed: false
+    }
+
+]
+
+export default todosData
